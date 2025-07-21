917c7d4819fb67d0f8148da64cc4fa449c87e19d
hannahgr425 <hannahreeman@outlook.com>
2025-07-21 13:53:02 +0100

    Update password_generator.py
    Co-Authored-By: jiabeijinn <221986105+jiabeijinn@users.noreply.github.com>

--- password_generator.py
+++ password_generator.py
@@ -47,33 +47,89 @@ def generate_password():
 
 print("Welcome to the password generator")
 generate=input("would you like to generate a new password? Y/N: ").lower()
+while generate not in ["y", "n"]:
+    print("please enter Y or N")
+    generate=input("would you like to generate a new password? Y/N: ").lower()
 while generate== "y":
-    print("password choices" \
-    "1. lower case" \
-    "2. upper case" \
-    "3. symbols" \
-    "4. numbers")
+    print("Password choices:\n" \
+    "1. lower case\n" \
+    "2. upper case\n" \
+    "3. symbols\n" \
+    "4. numbers\n" \
+    "-----------------------------------\nfor all options please enter Y or N if you want to include that option\n")
     toggle_input=False
     while toggle_input ==False:
-        choice_lower= input("lower case: Y/N: ").lower()
-        choice_upper= input("upper case: Y/N: ").lower()
-        choice_symbols= input("symbols: Y/N: ").lower()
-        choice_numbers= input("numbers: Y/N: ").lower()
+        choice_lower= input("lower case: ").lower()
+        while choice_lower not in ["y", "n"]:
+            print("please enter Y or N")
+            choice_lower= input("lower case: ").lower()
+        choice_upper= input("upper case: ").lower()
+        while choice_upper not in ["y", "n"]:
+            print("please enter Y or N")
+            choice_upper= input("upper case: ").lower()
+        choice_symbols= input("symbols: ").lower()
+        while choice_symbols not in ["y", "n"]:
+            print("please enter Y or N")
+            choice_symbols= input("symbols: ").lower()
+        choice_numbers= input("numbers: ").lower()
+        while choice_numbers not in ["y", "n"]:
+            print("please enter Y or N")
+            choice_numbers= input("numbers:").lower()
+        valid_length=False
+        while not valid_length:
+            try:
+                password_length=int(input("\n\npassword length: "))
+            except ValueError:
+                print("please enter an integer greater than one.")
+                password_length=int(input("\npassword length: "))
+            else:
+                valid_length=True
         choices = [choice_lower, choice_upper, choice_symbols, choice_numbers]
-        change=input("are you happy with your choices? Y/N: ").lower()
+        print ("\n--------------------------------------\n\nYou have chosen: \n"\
+               f"lower case: {choice_lower}\n"\
+                f"upper case: {choice_upper}\n"\
+                f"symbols: {choice_symbols}\n"\
+                f"numbers: {choice_numbers}\n"
+                f"password length: {password_length}\n")
+        change=input("are you happy with your choices? Y/N ").lower()
         while change not in ["y", "n"]:
             print("please enter Y or N")
+            change=input("are you happy with your choices? Y/N: ").lower()
         while change == "n":
-            choice_lower= input("lower case: Y/N: ").lower()
-            choice_upper= input("upper case: Y/N: ").lower()
-            choice_symbols= input("symbols: Y/N: ").lower()
-            choice_numbers= input("numbers: Y/N: ").lower()
- 
-        while change == "n":
-            choice=input("are you happy with your choices? Y/N: ").lower()
+            choice_lower= input("lower case: ").lower()
+            while choice_lower not in ["y", "n"]:
+                print("please enter Y or N")
+                choice_lower= input("lower case: ").lower()
+            choice_upper= input("upper case: ").lower()
+            while choice_upper not in ["y", "n"]:
+                print("please enter Y or N")
+                choice_upper= input("upper case: ").lower()
+            choice_symbols= input("symbols: ").lower()
+            while choice_symbols not in ["y", "n"]:
+                print("please enter Y or N")
+                choice_symbols= input("symbols: ").lower()
+            choice_numbers= input("numbers: ").lower()
+            while choice_numbers not in ["y", "n"]:
+                print("please enter Y or N")
+                choice_numbers= input("numbers: ").lower()
+            valid_length=False
+            while not valid_length:
+                try:
+                    password_length=int(input("\n\npassword length: "))
+                except ValueError:
+                    print("please enter an integer greater than one.")
+                    password_length=int(input("\npassword length: "))
+                else:
+                    valid_length=True
+            print ("you have chosen: \n"\
+               f"lower case: {choice_lower}\n"\
+                f"upper case: {choice_upper}\n"\
+                f"symbols: {choice_symbols}\n"\
+                f"numbers: {choice_numbers}\n"\
+                f"password length: {password_length}\n")
+            choice=input("are you happy with your choices? Y/N ").lower()
         if change == "y":
             toggle_input=True
-    password_length=int(input("password length: "))
     generate_password = generate_password()
-    print("generated password: ",generate_password)
-    generate=input("would you like to generate a new password? Y/N: ").lower()
+    print("\n\nGenerated password: ",generate_password)
+    generate=input("\nwould you like to generate a new password? Y/N: ").lower()
